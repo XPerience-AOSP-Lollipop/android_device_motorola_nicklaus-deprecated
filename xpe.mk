64a532ba21c130404e910a08ed5e811334c485d5
klozz <shaclone707@gmail.com>
2018-11-25 00:12:30 -0500

    nicklaus: Add missing mediatek_video bring xpe support
    Signed-off-by: klozz <shaclone707@gmail.com>

---
+++ xpe.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Inherit some common lineageos stuff.
+$(call inherit-product, vendor/XPe/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+PRODUCT_DEVICE := nicklaus
+PRODUCT_NAME := xpe_nicklaus
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := nicklaus
+PRODUCT_MANUFACTURER := nicklaus
